e706d1dba96401594b4216a9a01d1abe27efbb8b
camiarjona <camarjona01@gmail.com>
2024-11-03 18:14:35 -0300

    Metodos Json
    Metodos estaticos para guardar y leer jsons

---
+++ src/JSON/JSONUtiles.java
@@ -0,0 +1,39 @@
+package JSON;
+import org.json.*;
+
+import java.io.*;
+
+public class JSONUtiles {
+
+    public static void guardarJSONObject(JSONObject obj, String nombre) {
+        try(FileWriter file = new FileWriter(nombre)){
+            file.write(obj.toString(4));
+        }catch(IOException | JSONException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void guardarJSONArrayt(JSONArray array, String nombre) {
+        try(FileWriter file = new FileWriter(nombre)){
+            file.write(array.toString(4));
+        }catch(IOException | JSONException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static String leerArchivo(String nombre) {
+       StringBuilder contenido = new StringBuilder();
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(nombre));
+            String linea;
+
+            while((linea = br.readLine()) != null){
+                contenido.append(linea);
+            }
+            br.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return contenido.toString();
+    }
+}
